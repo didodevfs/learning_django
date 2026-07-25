0390d660469d3e997c33c204156674fd0a5458b1
Felipe Diniz <felipedinizse@gmail.com>
2026-07-25 17:33:35 -0300

    Changes all but one items of list 'cards' in index.html and iterates over the dictionary with the data in index fuction in views.py

--- gallery/views.py
+++ gallery/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'gallery/index.html')
+    
+    data = {
+        1: {"nome": "Nebulosa de Carina",
+            "legenda": "webbtelescope.org / NADA / James Webb"},
+        2: {"nome": "Galáxia NGC 1079",
+            "legenda": "nasa.org / NASA / Hubble"}
+        }
+
+    return render(request, 'gallery/index.html', {"cards": data})
 
 def image(request):
     return render(request, 'gallery/image.html')
