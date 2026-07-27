ccd184f4e55feaada4af1ee9363ea8bac77b252c
Felipe Diniz <felipedinizse@gmail.com>
2026-07-27 20:43:23 -0300

    first step with admin

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from gallery.models import Photograph
 
-# Register your models here.
+class ListingPhotographs(admin.ModelAdmin):
+    list_display = ("id", "name", "subtitle")
+    list_display_links = ("id", "name")
+    search_fields = ("name",)
+
+admin.site.register(Photograph, ListingPhotographs)
