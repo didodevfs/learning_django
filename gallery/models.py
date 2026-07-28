a0de837f4534e298fab1f3e8ae745fc60b3b06df
Felipe Diniz <felipedinizse@gmail.com>
2026-07-28 17:35:34 -0300

    Changes photo field to ImageField instead of Charfield, allowing user to upload directly from they pc

--- gallery/models.py
+++ gallery/models.py
@@ -14,7 +14,7 @@ class Photograph(models.Model):
     subtitle = models.CharField(max_length=100, null=False, blank=False)
     category = models.CharField(max_length=100, choices=CATEGORY_OPTIONS, default='')
     description = models.TextField(null=False, blank=False)
-    photo = models.CharField(max_length=100, null=False, blank=False)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True)
     published = models.BooleanField(default=False)
     photograph_date = models.DateTimeField(default=datetime.now, blank=False)
 
