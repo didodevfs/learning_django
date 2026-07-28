27d6826552403a22e469baea2d31b32422ff9bdb
Felipe Diniz <felipedinizse@gmail.com>
2026-07-28 12:54:02 -0300

    adds list_filter and list_per_page to admin's class ListingPhotographs

--- gallery/admin.py
+++ gallery/admin.py
@@ -5,5 +5,7 @@ class ListingPhotographs(admin.ModelAdmin):
     list_display = ("id", "name", "subtitle")
     list_display_links = ("id", "name")
     search_fields = ("name",)
+    list_filter = ("category",)
+    list_per_page = 10
 
 admin.site.register(Photograph, ListingPhotographs)
