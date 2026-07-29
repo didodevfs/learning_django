79b7d871621fca8f4b00b15390058703981fe3fe
Felipe Diniz <felipedinizse@gmail.com>
2026-07-29 11:03:55 -0300

    creates search feature

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from gallery.views import index, image
+from gallery.views import index, image, search
 
 urlpatterns = [
     path('', index, name='index'),
     path('image/<int:photo_id>', image, name='image'),
+    path('search', search, name='search'),
 ]
--- gallery/views.py
+++ gallery/views.py
@@ -3,10 +3,20 @@ from gallery.models import Photograph
 
 def index(request):
     
-    photographs = Photograph.objects.order_by("-photograph_date").filter(published=True) # aapenas adicionar um - ao argumento passado em "order_by" para apresentar em ordem decrescente
+    photographs = Photograph.objects.order_by("-photograph_date").filter(published=True) # apenas adicionar um - ao argumento passado em "order_by" para apresentar em ordem decrescente
 
     return render(request, 'gallery/index.html', {"cards": photographs})
 
 def image(request, photo_id):
     photograph = get_object_or_404(Photograph, pk=photo_id)
     return render(request, 'gallery/image.html', {"photograph": photograph})
+
+def search(request):
+    photographs = Photograph.objects.order_by("-photograph_date").filter(published=True)
+
+    if 'search' in request.GET: # vê se tem a palavra na url
+        searched_name = request.GET['search']
+        if searched_name:
+            photographs = photographs.filter(name__icontains=searched_name) # diferencia acento. Portanto não retorna quando diferente
+
+    return render(request, 'gallery/search.html', {"cards": photographs})
